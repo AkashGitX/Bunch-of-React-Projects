988dd9842d704041a497d751735b0914d23ee01d
AkashGitX <sutradharakash79@gmail.com>
2026-09-15 00:26:25 +0530

    Initial commit

---
+++ 01Basics/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Chai from './chai.jsx';
+
+function App() {
+  return (
+    <>
+    <h1>chai aur react</h1>
+    <Chai />
+    </>
+    
+  );
+}
+
+export default App;
